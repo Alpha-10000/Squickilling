5d68a792c691e9083551387d286ab997ddfda118
Alpha <mohamadou-alpha.abdoulaye@epita.fr>
2013-12-12 22:26:39 +0100

    Classe Sprite

---
+++ thegame/thegame/thegame/Sprite.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace thegame
+{
+
+
+    public class Sprite
+    {
+        /// <summary>
+        /// Récupère ou définit l'image du sprite
+        /// </summary>
+        public Texture2D Texture
+        {
+            get { return _texture; }
+            set { _texture = value; }
+        }
+        private Texture2D _texture;
+
+        /// <summary>
+        /// Récupère ou définit la position du Sprite
+        /// </summary>
+        public Vector2 Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        private Vector2 _position;
+
+        /// <summary>
+        /// Récupère ou définit la direction du sprite. Lorsque la direction est modifiée, elle est automatiquement normalisée.
+        /// </summary>
+        public Vector2 Direction
+        {
+            get { return _direction; }
+            set { _direction = Vector2.Normalize(value); }
+        }
+        private Vector2 _direction;
+
+        /// <summary>
+        /// Récupère ou définit la vitesse de déplacement du sprite.
+        /// </summary>
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+        private float _speed;
+    }
+
+
+}
