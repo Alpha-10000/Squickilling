5354f52eb6fa564b29fd7d1c75d183532adacb03
victorboissiere <victor.boissiere@gmail.com>
2014-06-16 13:31:40 +0100

    visual changes

--- thegame/thegame/thegame/create_game.cs
+++ thegame/thegame/thegame/create_game.cs
@@ -16,19 +16,20 @@ namespace thegame
         public bool goback = false;
 
         /* Content create */
-        private Rectangle titlecreate = new Rectangle(100, 77, 600, 50);
-        private Rectangle contentcreate = new Rectangle(100, 123, 600, 350);
+        private Rectangle contentcreate = new Rectangle(175, 70, 450, 220);
+        private Rectangle BottomBox = new Rectangle(50, 315, 700, 185);
         private bool finish = false;
 
         private string error;
         private List<Dictionary<string, string>> getFriends;
+        private List<Button> inviteButtonList = new List<Button>();
         private Popup popup;
 
         private Textbox essai_textbox;
 
         public Create_game()
         {
-            essai_textbox = new Textbox(50, 50, 200, 40);
+            essai_textbox = new Textbox(400, 75, 200, 40);
 
         }
 
@@ -40,10 +41,7 @@ namespace thegame
 
             if ((go_back.Contains(Inputs.getMousePoint()) && Inputs.isLMBClick()))
                 goback = true;
-          for(int i = 0; i<=2; i++)
-              if (new Rectangle(contentcreate.X + 300, contentcreate.Y + 50 + i * 50, 200, 40).Contains(Inputs.getMousePoint()) && Inputs.isLMBClick())
-                    popup = new Popup("OK", "", "Information", new string[] {"We are developping the functionnality"}, Textures.font_texture, 450);
-
+        
             if (popup != null)
             {
                 popup.Update(gametime);
@@ -52,6 +50,13 @@ namespace thegame
             }
 
             GetFriends(gametime);
+            if (getFriends != null)
+                for (int i = 0; i < getFriends.Count; i++)
+                {
+                    inviteButtonList[i].Update();
+                    if(inviteButtonList[i].Clicked)
+                        popup = new Popup("OK", "", "Information", new string[] { "We are developping the functionnality" }, Textures.font_texture, 450);
+                }
         }
 
         private void GetFriends(GameTime gametime)
@@ -85,6 +90,7 @@ namespace thegame
                 if (e.Result != null)
                 {
                     finish = true;
+                    inviteButtonList = new List<Button>();
                     string text = System.Text.Encoding.UTF8.GetString(e.Result);
 
 
@@ -92,6 +98,9 @@ namespace thegame
                     error = values["error"].ToString();
                     List<Dictionary<string, string>> ValueList = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(values["thearray"].ToString());
                     getFriends = ValueList;
+
+                    for (int i = 0; i < ValueList.Count; i++)
+                        inviteButtonList.Add(new Button("Invite", contentcreate.X + 300, contentcreate.Y + 50 + i * 50, Textures.fonthelp_texture));
                 }
             }
             catch
@@ -113,24 +122,31 @@ namespace thegame
 
             sb.DrawString(Textures.font_texture, "Go back", new Vector2(go_back.X, go_back.Y), Color.White);
 
-            sb.Draw(Textures.hitbox, titlecreate, Color.Beige);
-            Tools.DisplayBorder(sb, Color.Black, titlecreate, 4);
-            Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, "Create new game", AlignType.MiddleCenter, titlecreate);
-            sb.Draw(Textures.hitbox, contentcreate, Color.Beige);
-            Tools.DisplayBorder(sb, Color.Black, contentcreate, 4);
-            Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, "My friends", AlignType.MiddleCenter, new Rectangle(contentcreate.X, contentcreate.Y, contentcreate.Width, 50));
+            sb.Draw(Textures.hitbox, contentcreate, Color.Black * 0.4f);
+            Tools.DisplayBorder(sb, Color.White, contentcreate, 2);
+            Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "Search friends : ", AlignType.MiddleCenter, new Rectangle(contentcreate.X, contentcreate.Y, contentcreate.Width / 2, 50));
 
+            sb.Draw(Textures.hitbox, BottomBox, Color.Black * 0.4f);
+            Tools.DisplayBorder(sb, Color.White,  BottomBox, 2);
+
+            Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "Invited friends", AlignType.MiddleCenter, new Rectangle(BottomBox.X, BottomBox.Y, BottomBox.Width / 4, 50));
+            Tools.DisplayAlignedText(sb, Color.White, Textures.fonthelp_texture, "Nobody has been invited yet", AlignType.MiddleCenter, new Rectangle(BottomBox.X + 25, BottomBox.Y, BottomBox.Width / 3, BottomBox.Height));
 
             if (getFriends != null)
                 for (int i = 0; i < getFriends.Count; i++)
                 {
-                    Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, getFriends[i]["name"] + " : " + getFriends[i]["status"], AlignType.MiddleCenter, new Rectangle(contentcreate.X + 100, contentcreate.Y + 50 + i * 50, 100, 50));
-                    sb.Draw(Textures.hitbox, new Rectangle(contentcreate.X + 300, contentcreate.Y + 50 + i * 50, 200, 40), Color.Red);
-                    Tools.DisplayBorder(sb, Color.Black, new Rectangle(contentcreate.X + 300, contentcreate.Y + 50 + i * 50, 200, 40), 4);
-                    Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, "Invite", AlignType.MiddleCenter, new Rectangle(contentcreate.X + 300, contentcreate.Y + 50 + i * 50, 200, 40));
+                    Tools.DisplayAlignedText(sb, Color.White, Textures.fonthelp_texture, getFriends[i]["name"] + " : " + getFriends[i]["status"], AlignType.MiddleCenter, new Rectangle(contentcreate.X + 100, contentcreate.Y + 50 + i * 50, 100, 50));
+                    inviteButtonList[i].Display(sb);
+                    
                 }
 
             Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "Create game", AlignType.MiddleCenter, new Rectangle(0, 0, Game1.graphics.PreferredBackBufferWidth, 50));
+
+            Tools.DrawCircle(sb, 653, 405, 50, 600, Color.White, 120);
+            Tools.DisplayAlignedText(sb, Color.Black, Textures.fonthelp_texture, "Launch Game", AlignType.MiddleCenter, new Rectangle(653 - 50, 405 - 50, 100, 100));
+
+            sb.DrawString(Textures.fonthelp_texture, "Make it public", new Vector2(450, 390), Color.White);
+            sb.Draw(Textures.hitbox, new Rectangle(420, 392, 20, 20), Color.White);
 
             if (popup != null)
                 popup.Display(sb);
