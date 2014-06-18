f713bc70c8e65e03b48909e5e2d6e9b878d067e7
victorboissiere <victor.boissiere@gmail.com>
2014-06-18 17:05:45 +0100

    joined game not finish. Will be tonight

---
+++ thegame/thegame/thegame/HasJoined.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace thegame
+{
+    class HasJoined
+    {
+
+
+        private Rectangle TheBox = new Rectangle(50, 50, 750, 400);
+        private Button go_back;
+        public bool goback = false;
+
+        public HasJoined()
+        {
+            go_back = new Button("Go back", 620, 10, Textures.font_texture, new Color(122, 184, 0), Color.White, new Color(122, 184, 0));
+        }
+
+        public void Update(GameTime gametime)
+        {
+            go_back.Update();
+            if (go_back.Clicked)
+                goback = true;
+
+            
+        }
+
+        public void Display(SpriteBatch sb)
+        {
+            sb.Begin();
+            if (!Game1.graphics.IsFullScreen)
+                sb.Draw(Textures.menu_main_page, new Vector2(0, 0), Color.White);
+            else
+                sb.Draw(Textures.menu_main_page, new Rectangle(0, 0, Game1.graphics.PreferredBackBufferWidth + 40, Game1.graphics.PreferredBackBufferHeight + 5), Color.White);
+
+            go_back.Display(sb);
+
+
+
+            Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "Friends game", AlignType.MiddleCenter, new Rectangle(TheBox.X, TheBox.Y, TheBox.Width / 2, 60));
+            sb.Draw(Textures.hitbox, new Rectangle(TheBox.X, TheBox.Y + 52, TheBox.Width, 1), Color.White);
+
+            Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "You have joined a game", AlignType.MiddleCenter, new Rectangle(0, 0, Game1.graphics.PreferredBackBufferWidth, 50));
+
+
+
+            sb.End();
+        }
+
+
+    }
+}
--- thegame/thegame/thegame/Join_game.cs
+++ thegame/thegame/thegame/Join_game.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System.Net;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
 
 namespace thegame
 {
@@ -14,14 +17,20 @@ namespace thegame
 
 
         /* Content create */
-        private Rectangle titlejoin = new Rectangle(100, 77, 600, 50);
-        private Rectangle contentjoin = new Rectangle(100, 123, 600, 350);
+        private Rectangle contentjoin = new Rectangle(20, 70, 350, 350);
 
+        private List<Dictionary<string, string>> MyFriendsGame = new List<Dictionary<string, string>>();
+        private List<Button> JoinButton = new List<Button>();
+        private bool finish;
+        private float TimeInterval = 10000;
+        private float GameTimeGetInvitations;
 
-        private Popup popup;
+        public bool HasJoined = false;
 
         public Join_game()
         {
+            finish = true;
+            GameTimeGetInvitations = TimeInterval;
             go_back = new Button("Go back", 620, 10, Textures.font_texture, new Color(122, 184, 0), Color.White, new Color(122, 184, 0));
         }
 
@@ -31,19 +40,103 @@ namespace thegame
             if (go_back.Clicked)
                 goback = true;
 
-            for (int i = 0; i <= 2; i++)
-                if (new Rectangle(contentjoin.X + 300, contentjoin.Y + 50 + i * 50, 200, 40).Contains(Inputs.getMousePoint()) && Inputs.isLMBClick())
-                    popup = new Popup("OK", "", "Information", new string[] {"We are developping the functionnality"}, Textures.font_texture, 450);
+            
+           
+            GetInvitations(gametime);
 
-            if (popup != null)
+            if (JoinButton != null && JoinButton.Count > 0)
+                for (int i = 0; i < JoinButton.Count; i++)
+                {
+                    JoinButton[i].Update();
+                    if (JoinButton[i].Clicked)
+                        Join(MyFriendsGame[i]["otherid"]);
+                }
+        }
+
+        private void GetInvitations(GameTime gametime)
+        {
+            GameTimeGetInvitations += gametime.ElapsedGameTime.Milliseconds;
+            if (finish && GameTimeGetInvitations >= TimeInterval)
             {
-                popup.Update(gametime);
-                if (popup.action1bool)
-                    popup = null;
+                try
+                {
+                    GameTimeGetInvitations = 0;
+                    finish = false;
+                    WebClient wb = new WebClient();
+                    var data = new NameValueCollection();
+                    wb.UploadValuesCompleted += new UploadValuesCompletedEventHandler(client_UploadFileCompleted);
+                    data["pwd"] = Session.session_password;
+                    data["id"] = Session.session_id;
+                    wb.UploadValuesAsync(new Uri("http://squickilling.com/json/list_join.php"), "POST", data);
+                }
+                catch
+                {
+                    //TODO
+                }
             }
         }
 
+        void client_UploadFileCompleted(object sender, UploadValuesCompletedEventArgs e)
+        {
+            try
+            {
 
+                if (e.Result != null)
+                {
+                    finish = true;
+                    JoinButton = new List<Button>();
+                    string text = System.Text.Encoding.UTF8.GetString(e.Result);
+                    Dictionary<string, object> values = JsonConvert.DeserializeObject<Dictionary<string, object>>(text);
+                    List<Dictionary<string, string>> ValueList = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(values["thearray"].ToString());
+                    MyFriendsGame = ValueList;
+
+                    for (int i = 0; i < ValueList.Count; i++)
+                        JoinButton.Add(new Button("Join game", contentjoin.X + 128, contentjoin.Y + 115 + i * 100, Textures.font_texture, new Color(129, 130, 134), Color.White, new Color(14, 15, 15)));
+
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Join(string otherid)
+        {
+                try
+                {
+                    GameTimeGetInvitations = 0;
+                    finish = false;
+                    WebClient wb = new WebClient();
+                    var data = new NameValueCollection();
+                    wb.UploadValuesCompleted += new UploadValuesCompletedEventHandler(client_UploadFileCompleted2);
+                    data["pwd"] = Session.session_password;
+                    data["id"] = Session.session_id;
+                    data["otherid"] = otherid;
+                    wb.UploadValuesAsync(new Uri("http://squickilling.com/json/join_game.php"), "POST", data);
+                }
+                catch
+                {
+                    //TODO
+                }
+        }
+
+        void client_UploadFileCompleted2(object sender, UploadValuesCompletedEventArgs e)
+        {
+            try
+            {
+                if (e.Result != null)
+                {
+                    string text = System.Text.Encoding.UTF8.GetString(e.Result);
+                    if (text == "Success")
+                        HasJoined = true;
+                }
+            }
+            catch
+            {
+
+            }
+        }
 
         public void Display(SpriteBatch sb)
         {
@@ -55,26 +148,29 @@ namespace thegame
 
             go_back.Display(sb);
 
-            sb.Draw(Textures.hitbox, titlejoin, Color.Beige);
-            Tools.DisplayBorder(sb, Color.Black, titlejoin, 4);
-            Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, "Join current game", AlignType.MiddleCenter, titlejoin);
-            sb.Draw(Textures.hitbox, contentjoin, Color.Beige);
-            Tools.DisplayBorder(sb, Color.Black, contentjoin, 4);
-            Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, "The game created by my friends", AlignType.MiddleCenter, new Rectangle(contentjoin.X, contentjoin.Y, contentjoin.Width, 50));
-
-            for (int i = 0; i <= 2; i++)
+          
+            sb.Draw(Textures.hitbox, contentjoin, Color.Black * 0.4f);
+            Tools.DisplayBorder(sb, Color.White, contentjoin, 2);
+            Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "Friends game", AlignType.MiddleCenter, new Rectangle(contentjoin.X, contentjoin.Y, contentjoin.Width / 2, 60));
+            sb.Draw(Textures.hitbox, new Rectangle(contentjoin.X, contentjoin.Y + 52, contentjoin.Width, 1), Color.White);
+            if (MyFriendsGame.Count == 0)
             {
-                Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, "game " + i + " by [...]", AlignType.MiddleCenter, new Rectangle(contentjoin.X + 100, contentjoin.Y + 50 + i * 50, 100, 50));
-                sb.Draw(Textures.hitbox, new Rectangle(contentjoin.X + 300, contentjoin.Y + 50 + i * 50, 200, 40), Color.Red);
-                Tools.DisplayBorder(sb, Color.Black, new Rectangle(contentjoin.X + 300, contentjoin.Y + 50 + i * 50, 200, 40), 4);
-                Tools.DisplayAlignedText(sb, Color.Black, Textures.font_texture, "Join", AlignType.MiddleCenter, new Rectangle(contentjoin.X + 300, contentjoin.Y + 50 + i * 50, 200, 40));
+                Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "No games at the time", AlignType.MiddleCenter, new Rectangle(contentjoin.X, contentjoin.Y + 65, contentjoin.Width, 60));
+                Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "Try to add some friends", AlignType.MiddleCenter, new Rectangle(contentjoin.X, contentjoin.Y + 100, contentjoin.Width, 60));
+            }
+            else
+            {
+                for(int i = 0; i < MyFriendsGame.Count; i++)
+                    Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, MyFriendsGame[i]["name"] + " has invited you", AlignType.MiddleCenter, new Rectangle(contentjoin.X, contentjoin.Y + 65 + i * 60, contentjoin.Width, 60));
             }
 
+            if (JoinButton != null && JoinButton.Count > 0)
+                for (int i = 0; i < JoinButton.Count; i++)
+                    JoinButton[i].Display(sb);
 
             Tools.DisplayAlignedText(sb, Color.White, Textures.font_texture, "Join game", AlignType.MiddleCenter, new Rectangle(0, 0, Game1.graphics.PreferredBackBufferWidth, 50));
 
-            if (popup != null)
-                popup.Display(sb);
+            
 
             sb.End();
         }
