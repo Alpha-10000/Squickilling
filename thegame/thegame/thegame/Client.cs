ab5693ea287b5a64ba7fa68cafefccf1857a7883
victorboissiere <victor.boissiere@gmail.com>
2014-06-23 13:32:51 +0200

    changes server

--- thegame/thegame/thegame/Client.cs
+++ thegame/thegame/thegame/Client.cs
@@ -57,7 +57,7 @@ namespace thegame
             outmsg.Write((byte)PacketTypes.LOGIN);
             outmsg.Write(theid);
             //outmsg.Write("osef");
-            string ip = "127.0.0.1";
+            string ip = "37.187.43.213";
             client.Connect(ip, 14242, outmsg);
             p = new Perso(new Vector2(0, 200), CharacType.player);
             p.utilisable = true;
